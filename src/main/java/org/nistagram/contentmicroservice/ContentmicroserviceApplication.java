3418a5f3471e0e2b14288e44d2a8454092e74786
Milica Siriski <milica.siriski998@gmail.com>
2021-05-05 17:16:30 +0200

    Project init

---
+++ src/main/java/org/nistagram/contentmicroservice/ContentmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.nistagram.contentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentmicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentmicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nistagram/contentmicroservice/ContentmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nistagram.contentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentmicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
