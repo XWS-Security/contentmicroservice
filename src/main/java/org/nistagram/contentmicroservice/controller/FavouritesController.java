d62ab004d218e67299895266d10c6f787d210ef8
Vladimir Budjen <bvalda88@gmail.com>
2021-06-07 19:43:17 +0200

    Made backend for savet posts, but dindn't test it

---
+++ src/main/java/org/nistagram/contentmicroservice/controller/FavouritesController.java
@@ -0,0 +1,2 @@
+package org.nistagram.contentmicroservice.controller;public class FavouritesController {
+}
---
+++ src/main/java/org/nistagram/contentmicroservice/service/IFavouritesService.java
@@ -0,0 +1,2 @@
+package org.nistagram.contentmicroservice.service;public interface IFavouritesService {
+}
