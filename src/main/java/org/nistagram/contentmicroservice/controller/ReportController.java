d8fd92b889054cae9b33d6ccb09bd57f70f91da5
lukaDoric <luka.doric@uns.ac.rs>
2021-06-14 16:24:49 +0200

    Minor fix in Report model.

--- src/main/java/org/nistagram/contentmicroservice/controller/ReportController.java
+++ src/main/java/org/nistagram/contentmicroservice/controller/ReportController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +27,13 @@ public class ReportController {
 
     @PreAuthorize("hasAuthority('NISTAGRAM_USER_ROLE')")
     @PostMapping(path = "/")
-    public ResponseEntity<String> createReport(CreateReportDto createReportDto) {
-        reportService.createReport(createReportDto);
-        return new ResponseEntity<>("Content reported successfully!", HttpStatus.OK);
+    public ResponseEntity<String> createReport(@RequestBody CreateReportDto createReportDto) {
+        try {
+            reportService.createReport(createReportDto);
+            return new ResponseEntity<>("Content reported successfully!", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Report didn't send!", HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/org/nistagram/contentmicroservice/service/impl/ReportServiceImpl.java
+++ src/main/java/org/nistagram/contentmicroservice/service/impl/ReportServiceImpl.java
@@ -1,14 +1,31 @@
 package org.nistagram.contentmicroservice.service.impl;
 
 import org.nistagram.contentmicroservice.data.dto.CreateReportDto;
+import org.nistagram.contentmicroservice.data.model.NistagramUser;
+import org.nistagram.contentmicroservice.data.model.Report;
+import org.nistagram.contentmicroservice.data.model.content.Content;
+import org.nistagram.contentmicroservice.data.repository.ContentRepository;
+import org.nistagram.contentmicroservice.data.repository.ReportRepository;
 import org.nistagram.contentmicroservice.service.ReportService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportServiceImpl implements ReportService {
 
+    private final ReportRepository reportRepository;
+    private final ContentRepository contentRepository;
+
+    public ReportServiceImpl(ReportRepository reportRepository, ContentRepository contentRepository) {
+        this.reportRepository = reportRepository;
+        this.contentRepository = contentRepository;
+    }
+
     @Override
     public void createReport(CreateReportDto createReportDto) {
-
+        Content content = contentRepository.findById(createReportDto.getContentId()).get();
+        NistagramUser user = (NistagramUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Report report = new Report(createReportDto.getReason(), content, user);
+        reportRepository.save(report);
     }
 }
