5df46a90ca2babeba4aa83c2f7b3643895cec3c4
Milica Siriski <milica.siriski998@gmail.com>
2021-06-21 12:51:58 +0200

    Queries fixed, added responses to content upload

--- src/main/java/org/nistagram/contentmicroservice/data/repository/NistagramUserRepository.java
+++ src/main/java/org/nistagram/contentmicroservice/data/repository/NistagramUserRepository.java
@@ -10,21 +10,21 @@ public interface NistagramUserRepository extends CrudRepository<NistagramUser, L
     NistagramUser findByUsername(String username);
 
     @Query(value = "SELECT DISTINCT u.id, u.user_type, u.username, u.enabled, u.last_password_reset_date, u.profile_picture," +
-            " u.about, u.private_profile FROM nistagram_user AS u, content AS c" +
+            " u.about, u.private_profile, u.tags_enabled FROM nistagram_user AS u, content AS c" +
             " WHERE u.user_type = 'INSTAGRAM_USER' AND u.id = c.user_id AND c.id = :id", nativeQuery = true)
     NistagramUser findByContentContaining(Long id);
 
     @Query(value = "SELECT DISTINCT u.id, u.user_type, u.username, u.enabled, u.last_password_reset_date, u.profile_picture," +
-            " u.about, u.private_profile FROM nistagram_user AS u, content AS c, post_likes AS pl" +
+            " u.about, u.private_profile, u.tags_enabled FROM nistagram_user AS u, content AS c, post_likes AS pl" +
             " WHERE u.user_type = 'INSTAGRAM_USER' AND u.id = pl.user_id AND c.id = pl.post_id AND c.id = :id", nativeQuery = true)
     List<NistagramUser> findLikesForPost(long id);
 
     @Query(value = "SELECT DISTINCT u.id, u.user_type, u.username, u.enabled, u.last_password_reset_date, u.profile_picture," +
-            " u.about, u.private_profile FROM nistagram_user AS u, content AS c, post_dislikes AS pd" +
+            " u.about, u.private_profile, u.tags_enabled FROM nistagram_user AS u, content AS c, post_dislikes AS pd" +
             " WHERE u.user_type = 'INSTAGRAM_USER' AND u.id = pd.user_id AND c.id = pd.post_id AND c.id = :id", nativeQuery = true)
     List<NistagramUser> findDislikesForPost(long id);
 
     @Query(value = "SELECT DISTINCT u.id, u.user_type, u.username, u.enabled, u.last_password_reset_date, u.profile_picture," +
-            " u.about, u.private_profile FROM nistagram_user AS u, user_close_friend AS ucf WHERE ucf.user_id = :id AND ucf.close_friend_id = u.id", nativeQuery = true)
+            " u.about, u.private_profile, u.tags_enabled FROM nistagram_user AS u, user_close_friend AS ucf WHERE ucf.user_id = :id AND ucf.close_friend_id = u.id", nativeQuery = true)
     List<NistagramUser> getCloseFriends(Long id);
 }
