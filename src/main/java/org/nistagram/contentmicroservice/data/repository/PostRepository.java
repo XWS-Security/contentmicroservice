e0644a2f79e99b9297e51d5c0d9de22dd14acb82
Milica Siriski <milica.siriski998@gmail.com>
2021-06-24 18:13:53 +0200

    Fixed favorites query

--- src/main/java/org/nistagram/contentmicroservice/data/repository/PostRepository.java
+++ src/main/java/org/nistagram/contentmicroservice/data/repository/PostRepository.java
@@ -11,8 +11,7 @@ public interface PostRepository extends CrudRepository<Post, Long> {
     @Query(value = "SELECT * FROM content WHERE content_type = 'POST' AND user_id = :id", nativeQuery = true)
     List<Post> findAllByUserId(@Param("id") Long id);
 
-    @Query(value = "SELECT c.id, c.tags, c.date, c.location_id, c.paths, c.about, c.content_type, c.deleted " +
-            " FROM content AS c, user_saved AS us" +
-            " WHERE us.user_id = :id AND us.saved_id = c.id AND c.deleted = false", nativeQuery = true)
+    @Query(value = "SELECT c.id, c.tags, c.date, c.location_id, c.paths, c.about, c.content_type, c.user_id " +
+            "FROM content AS c, user_saved AS us WHERE us.user_id = :id AND us.saved_id = c.id", nativeQuery = true)
     List<Post> findAllFavourites(@Param("id") Long id);
 }
