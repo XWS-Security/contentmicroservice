0b7711be97569564e3e41143b79e4d663af9fd91
Vladimir Budjen <bvalda88@gmail.com>
2021-07-10 02:10:51 +0200

    Campaign service can get basic content info

--- src/main/java/org/nistagram/contentmicroservice/service/ContentService.java
+++ src/main/java/org/nistagram/contentmicroservice/service/ContentService.java
@@ -1,9 +1,12 @@
 package org.nistagram.contentmicroservice.service;
 
 import org.nistagram.contentmicroservice.data.dto.ContentMessageInfoDto;
+import org.nistagram.contentmicroservice.data.dto.LikesDislikesCommentsDto;
 
 import javax.net.ssl.SSLException;
 
 public interface ContentService {
     ContentMessageInfoDto getContentInfo(long id, String token) throws SSLException;
+
+    LikesDislikesCommentsDto getLikesDislikesAndComments(Long id);
 }
